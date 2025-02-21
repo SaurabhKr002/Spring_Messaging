3491cd5ef96188a92075c41727ffe158421e20b5
Saurabh Kumar <129621065+SaurabhKr002@users.noreply.github.com>
2025-02-21 22:26:28 +0530

    Saurabh add . using PUT to show hello bridgelabz

---
+++ FirstSpringAppApplication/src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,48 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+    // UC1: Basic GET Request
+    @GetMapping
+    public String sayHello() {
+        logger.info("GET /hello called");
+        return "Hello From BridgeLabz!!!";
+    }
+
+    // UC2: GET Request with Query Parameter
+    @GetMapping("/query")
+    public String sayHelloQuery(@RequestParam(value = "name") String name) {
+        logger.info("GET /hello/query called with name: {}", name);
+        return "Hello " + name + " from BridgeLabz!";
+    }
+
+    // UC3: GET Request with Path Variable
+    @GetMapping("/param/{name}")
+    public String sayHelloParam(@PathVariable String name) {
+        logger.info("GET /hello/param/{} called", name);
+        return "Hello " + name + " from BridgeLabz!";
+    }
+
+    // UC4: POST Request with JSON Body
+    @PostMapping("/post")
+    public String sayHelloPost(@RequestBody User user) {
+        logger.info("POST /hello/post called with user: {} {}", user.getFirstName(), user.getLastName());
+        return "Hello " + user.getFirstName() + " " + user.getLastName() + " from BridgeLabz!";
+    }
+
+    // UC5: PUT Request with Path Variable & Query Parameter
+    @PutMapping("/put/{firstName}")
+    public String sayHelloPut(@PathVariable String firstName, @RequestParam(value = "lastName") String lastName) {
+        logger.info("PUT /hello/put/{}?lastName={} called", firstName, lastName);
+        return "Hello " + firstName + " " + lastName + " from BridgeLabz!";
+    }
+}
