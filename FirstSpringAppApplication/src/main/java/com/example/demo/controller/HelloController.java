5a26e8a9b57973fbd4e7800d262529ff5a44c650
Saurabh Kumar <129621065+SaurabhKr002@users.noreply.github.com>
2025-02-21 22:10:27 +0530

    Saurabh add . request method and pass name as variable

---
+++ FirstSpringAppApplication/src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,33 @@
+package com.example.demo.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+    // UC1: Basic GET Request
+    @GetMapping
+    public String sayHello() {
+        logger.info("GET /hello called");
+        return "Hello From BridgeLabz!!!";
+    }
+
+    // UC2: GET Request with Query Parameter
+    @GetMapping("/query")
+    public String sayHelloQuery(@RequestParam(value = "name") String name) {
+        logger.info("GET /hello/query called with name: {}", name);
+        return "Hello " + name + " from BridgeLabz!";
+    }
+
+    // UC3: GET Request with Path Variable
+    @GetMapping("/param/{name}")
+    public String sayHelloParam(@PathVariable String name) {
+        logger.info("GET /hello/param/{} called", name);
+        return "Hello " + name + " from BridgeLabz!";
+    }
+}
